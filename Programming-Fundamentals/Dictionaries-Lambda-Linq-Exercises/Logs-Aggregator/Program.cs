d9962360dd5e3c0c3a62ea897cc5d5ad3b5cd3c3
kalina957 <kalina957@users.noreply.github.com>
2018-06-28 21:46:27 +0300

    Committing Dictionaries,Lambda,Linq -Excersices

--- Programming-Fundamentals/Dictionaries-Lambda-Linq-Exercises/Logs-Aggregator/Program.cs
+++ Programming-Fundamentals/Dictionaries-Lambda-Linq-Exercises/Logs-Aggregator/Program.cs
@@ -8,50 +8,8 @@ namespace Logs_Aggregator
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            SortedDictionary<string, SortedDictionary<string, int>> userLogs = new SortedDictionary<string, SortedDictionary<string, int>>();
-
-
-            for (int i = 0; i < n; i++)
-            {
-                string[] input = Console.ReadLine().Split();
-                string ip = input[0];
-                string userName = input[1];
-                int duration = int.Parse(input[2]);
-
-                InsertUser(userLogs,userName);
-                InsertIPAndDuration(userLogs,userName,ip,duration);
-            }
-
-            PrintData(userLogs);
-        }
-
-        private static void PrintData(SortedDictionary<string, SortedDictionary<string, int>> userLogs)
-        {
-            foreach (var input in userLogs)
-            {
-                string userName = input.Key;
-                int duration = input.Value.Values.Sum();
-                List<string> ip = input.Value.Keys.ToList();
-                Console.WriteLine("{0}: {1} [{2}]" , userName,duration, String.Join(", ",ip));
-            }
-        }
-
-        private static void InsertIPAndDuration(SortedDictionary<string, SortedDictionary<string, int>> userLogs, string userName, string ip, int duration)
-        {
-            if (!userLogs[userName].ContainsKey(ip))
-            {
-                userLogs[userName].Add(ip,0);
-            }
-            userLogs[userName][ip] += duration;
-        }
-
-        private static void InsertUser(SortedDictionary<string, SortedDictionary<string, int>> userLogs ,string userName)
-        {
-            if (!userLogs.ContainsKey(userName))
-            {
-                userLogs.Add(userName, new SortedDictionary<string, int>());
-            };
+            string neshtosi = "1    2";
+            Console.WriteLine(string.Join("",neshtosi.Split(' ')));
         }
     }
 }
