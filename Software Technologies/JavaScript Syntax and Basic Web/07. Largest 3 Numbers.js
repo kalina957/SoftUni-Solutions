c389f1e496f1809c8789f8dde89c4477e5902b25
kalina957 <kalina957@users.noreply.github.com>
2018-07-27 14:54:41 +0300

    c

---
+++ Software Technologies/JavaScript Syntax and Basic Web/07. Largest 3 Numbers.js
@@ -0,0 +1,14 @@
+function largest3Numbers(arr) {
+
+    let nums = arr.map(Number);
+
+    let numsSorted = nums.sort((a, b) => b - a);
+
+    let count = Math.min(3, arr.length);
+
+    for (let i = 0; i < count; i++)
+
+        console.log(numsSorted[i]);
+
+}
+//largest3Numbers(['10', '30', '15', '20', '50', '5'])
