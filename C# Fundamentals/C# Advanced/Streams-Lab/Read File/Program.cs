53d5ea9f12b30e0ed182c9d3919b7fe570eb5445
kalina957 <kalina957@users.noreply.github.com>
2018-08-09 12:06:05 +0300

    Committing Streams-Lab

---
+++ C# Fundamentals/C# Advanced/Streams-Lab/Read File/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+
+namespace Read_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
