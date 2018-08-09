f3ebb359e907148b7ca7f4eb9f0f98cad5ba4a26
kalina957 <kalina957@users.noreply.github.com>
2018-08-09 15:35:49 +0300

    Committing Streams-Lab

--- C# Fundamentals/C# Advanced/Streams-Lab/Read File/Program.cs
+++ C# Fundamentals/C# Advanced/Streams-Lab/Read File/Program.cs
@@ -8,7 +8,16 @@ namespace Read_File
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var stream = new StreamReader(@"C:\Users\User\Desktop\SoftUni-Solutions\SoftUni-Solutions\C# Fundamentals\C# Advanced\Streams-Lab\Read File\Program.cs"))
+            {
+                var lineNumber = 1;
+                string line ;
+
+                while ((line = stream.ReadLine())!=null)
+                {
+                    Console.WriteLine($"Line {lineNumber}: "+line);
+                }
+            }
         }
     }
 }
