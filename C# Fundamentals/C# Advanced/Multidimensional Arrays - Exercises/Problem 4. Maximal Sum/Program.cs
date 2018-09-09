8b6118e107ec483775a234d9d1576f5bdebbb8a9
kalina957 <kalina957@users.noreply.github.com>
2018-09-09 14:59:19 +0200

    Committing Multidimensional Arrays - Exercises

--- C# Fundamentals/C# Advanced/Multidimensional Arrays - Exercises/Problem 4. Maximal Sum/Program.cs
+++ C# Fundamentals/C# Advanced/Multidimensional Arrays - Exercises/Problem 4. Maximal Sum/Program.cs
@@ -14,7 +14,7 @@ namespace Problem_4._Maximal_Sum
 
             for (int r = 0; r < rows; r++)
             {
-                int[] row = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int[] row = Console.ReadLine().Split(new string[] {" " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
 
                 for (int c = 0; c < cols; c++)
                 {
@@ -23,13 +23,39 @@ namespace Problem_4._Maximal_Sum
             }
             int maxSum = 0;
             int currSum = 0;
+            int maxI = 0;
+            int maxR = 0;
 
             for (int r = 0; r < matrix.GetLength(0)-2; r++)
             {
                 for (int c = 0; c < matrix.GetLength(1) - 2; c++)
                 {
 
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            currSum += matrix[r+i, c+j];
+                        }
+                    }
+                    if (currSum > maxSum)
+                    {
+                        maxSum = currSum;
+                        maxI = r;
+                        maxR = c;
+                    }
+                    currSum = 0;
                 }
+            }
+            Console.WriteLine($"Sum = {maxSum}");
+
+            for (int i = maxI; i <maxI+3; i++)
+            {
+                for (int j = maxR; j < maxR+3; j++)
+                {
+                    Console.Write(matrix[i,j] + " ");
+                }
+                Console.WriteLine();
             }
         }
     }
